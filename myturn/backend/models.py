16ff32ff7873b6f05cca5798b2dfeced6bf8fc55
firehellrain <firehellrain@gmail.com>
2022-03-26 00:01:24 +0100

    Models config
    Added Meeting and Turn models and created them on the database.
    
    Added both to the admin panel

--- myturn/backend/models.py
+++ myturn/backend/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Meeting(models.Model):
+    meeting_id = models.IntegerField(default=0)
+    meeting_name = models.CharField(max_length=50)
+    meeting_mod = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.meeting_id)
+
+class Turn(models.Model):
+    meeting_turns = models.ForeignKey(Meeting, on_delete=models.CASCADE)
+    turn_type = models.CharField(max_length=20)
+    turn_user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.turn_type + ", " + self.turn_user.username
