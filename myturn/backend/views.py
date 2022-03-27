27b912f2b4437c9b99768ca7bdaf1239466807cd
firehellrain <firehellrain@gmail.com>
2022-03-27 13:59:09 +0200

    (B) Specify login error

--- myturn/backend/views.py
+++ myturn/backend/views.py
@@ -48,7 +48,7 @@ def loginUser(request):
         login(request, user)
         return Response(status=status.HTTP_200_OK)
     else:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return response("Las credenciales de inicio de sesión no son válidas", status=status.HTTP_404_NOT_FOUND)
 
 @api_view(('GET',))
 def logoutUser(request):
