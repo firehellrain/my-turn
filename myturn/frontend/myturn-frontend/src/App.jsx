33ebc960853127936dc0c24ba8f6f4eaa8dedff5
Blackwolf4183 <aperezvic21@gmail.com>
2022-03-27 19:12:19 +0200

    (F) Added auth expiration

--- myturn/frontend/myturn-frontend/src/App.jsx
+++ myturn/frontend/myturn-frontend/src/App.jsx
@@ -1,7 +1,12 @@
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+  Navigate,
+} from "react-router-dom";
 import { AuthContext } from "./shared/context/auth-context";
 
-import { useEffect, useContext } from "react";
+import { useEffect, useCallback, useState } from "react";
 
 import Login from "./Auth/Login";
 import Dashboard from "./Dashboard/Dashboard";
@@ -9,22 +14,71 @@ import Footer from "./shared/components/Footer";
 import Navbar from "./shared/components/Navbar";
 
 function App() {
-  const auth = useContext(AuthContext);
+  const [token, setToken] = useState(null);
 
-  useEffect(() => {
-    console.log(auth.isLoggedIn);
+  const login = useCallback((token, expirationDate) => {
+    setToken(token);
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60 * 24); // 1 dia de expiración de token
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        token: token,
+        expiration: tokenExpirationDate.toISOString(),
+      })
+    );
   }, []);
 
+  const logout = useCallback(() => {
+    setToken(null);
+    setUserId(null);
+    localStorage.removeItem("userData");
+  }, []);
 
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(storedData.token, new Date(storedData.expiration));
+    }
+  }, [login]);
+
+  let routes;
+
+  if (token) {
+    //usuario logeado
+    routes = (
+      <Routes>
+        <Route path="/" exact element={<Login />} />
+        <Route path="/main" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    );
+  } else {
+    //usuario no logeado
+    routes = (
+      <Routes>
+        <Route path="/main" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/main" replace />} />
+      </Routes>
+    );
+  }
 
   return (
-    <AuthContext.Provider value={{isLoggedIn: false}}>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: !!token, //true if there's no token
+        token: token,
+        login: login,
+        logout: logout,
+      }}
+    >
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Login />} />
-          <Route path="/main" element={<Dashboard />} />
-        </Routes>
+        {routes}
         <Footer />
       </Router>
     </AuthContext.Provider>
