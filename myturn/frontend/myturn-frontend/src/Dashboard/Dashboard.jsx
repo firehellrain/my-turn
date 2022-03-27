bd49951590c9ac7a552f8b19ea1e96abd72445ec
Blackwolf4183 <aperezvic21@gmail.com>
2022-03-27 15:52:01 +0200

    Started work on main page and changes on navbar

--- myturn/frontend/myturn-frontend/src/Dashboard/Dashboard.jsx
+++ myturn/frontend/myturn-frontend/src/Dashboard/Dashboard.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { VStack, Heading,HStack,PinInput,PinInputField,Button, Input } from "@chakra-ui/react";
+import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <VStack>
+      <Heading>Ingresa en una reunión</Heading>
+      <HStack>
+        <PinInput >
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+        </PinInput>
+      </HStack>
+      <Button variant="outline">Entrar</Button>
+      <Heading>Crea una nueva</Heading>
+      <Input type="text" maxLength={20} w="200px"/>
+      <Button variant="outline">Crear</Button>
+      <Heading fontSize="3xl">ESTO ES TEMPORAL PARA IR VIENDO NO ES EL DISEÑO FINAL ^^</Heading>
+    </VStack>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
