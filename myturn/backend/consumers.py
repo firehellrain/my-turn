8b8f3e4e8dc5e44f8197b9b99a5adeb1688a5d43
firehellrain <firehellrain@gmail.com>
2022-03-29 01:12:52 +0200

    (B) Created Sync WebSocket
    - Configured Web Socket to work syncronously
    - Added turn list request for the meeting where it has been requested, which is sent to all users on the meeting
    - (TO DO) Added add_turn request which allows a given user to add a turn to a meeting

--- myturn/backend/consumers.py
+++ myturn/backend/consumers.py
@@ -1,44 +1,79 @@
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from rest_framework.authtoken.models import Token
+from .models import Meeting
 
-class MeetingConsumer(AsyncWebsocketConsumer):
+class MeetingConsumer(WebsocketConsumer):
 
-    async def connect(self):
-        self.room_group_name = self.scope['url_route']['kwargs']['meeting_id']
-
+    def connect(self):
+        self.meeting_code = self.scope['url_route']['kwargs']['meeting_id']
+        self.meeting = Meeting.objects.get(meeting_id=self.meeting_code)
+        
         # Unirse a la reunión
-        await self.channel_layer.group_add(
-            self.room_group_name,
+        async_to_sync(self.channel_layer.group_add)(
+            self.meeting_code,
             self.channel_name
         )
 
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.room_group_name,
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.meeting_code,
             self.channel_name
         )
 
     # Recibir mensaje del WebSocket
-    async def receive(self, text_data):
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        request = text_data_json['request']
 
-        # Llama a la función para enviar el mensaje
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
+        if request == "get_turn_list":
+            async_to_sync(self.channel_layer.group_send)(
+                self.meeting_code,
+                {
+                    'type': request,
+                }
+            )
+        elif request == "add_turn":
+            async_to_sync(self.channel_layer.group_send)(
+                self.meeting_code,
+                {
+                    'type': request,
+                    'turn_type': text_data_json['turn_type'],
+                    'user_token': text_data_json['token_key']
+                }
+            )
+        elif request == "delete_turn":
+            async_to_sync(self.channel_layer.group_send)(
+                self.meeting_code,
+                {
+                    'type': request,
+                    'turn_id': text_data_json['turn_id'],
+                }
+            )
 
-    # Enviar mensaje al grupo
-    async def chat_message(self, event):
-        message = event['message']
-
-        await self.send(text_data=json.dumps({
-            'message': message,
-            'refresh_turns': True,
+    # Envia la lista de turnos al grupo
+    def get_turn_list(self, event):
+        self.send(text_data=json.dumps({
+            'turn_list': list(self.meeting.turn_set.all().values()),
         }))
+
+    def add_turn(self, event):
+        try:
+            token = Token.objects.get(key=event['token_key'])
+            meeting = Meeting.objects.get(meeting_id=self.meeting)
+            meeting.turn_set.create(turn_type=event['turn_type'], turn_user=token.user)
+            self.send(text_data=json.dumps({
+            'turn_list': list(self.meeting.turn_set.all().values()),
+        }))
+        except: 
+            self.send(text_data=json.dumps({
+                'error': 'Peticion denegada, algo salio mal.'
+            }))
+        
+
+   
+
+    
