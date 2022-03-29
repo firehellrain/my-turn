c8e5afb205a20daaed9c364431d36080be6861a2
Blackwolf4183 <aperezvic21@gmai.com>
2022-03-29 12:10:42 +0200

    (F) Stated work on main page

--- myturn/frontend/myturn-frontend/src/Dashboard/Dashboard.jsx
+++ myturn/frontend/myturn-frontend/src/Dashboard/Dashboard.jsx
@@ -1,12 +1,15 @@
 import {
   VStack,
   Heading,
-  HStack,
-  PinInput,
-  PinInputField,
   Button,
   Input,
   useColorMode,
+  Text,
+  Grid,
+  GridItem,
+  Image,
+  Center,
+  Icon,
 } from "@chakra-ui/react";
 import React from "react";
 
@@ -14,79 +17,63 @@ import { AuthContext } from "../shared/context/auth-context";
 import { useContext, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
+import Illustration from "../assets/dashboardImg.svg";
+
 const Dashboard = () => {
   const auth = useContext(AuthContext);
   const history = useHistory();
 
   return (
-    <VStack spacing="10" mt={["5vh","10vh"]}>
-      <VStack
-        spacing="5"
-        textAlign={"center"}
-        w="300px"
-        h="auto"
-        borderRadius={"md"}
-        bgColor={"primary"}
-        p="20px"
-        
-        boxShadow="rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;"
-      >
-        <Heading fontSize="2xl">Ingresa en una reunión</Heading>
-        <HStack>
-          <PinInput size="sm" >
-            <PinInputField
-              borderWidth={"0 0 3px 0"}
-              borderRadius="0"
-              fontSize="xl"
-              fontWeight={"600"}
-              _focus={{ borderWidth: "0 0 3px 0" }}
+    <Grid templateColumns={"repeat(2,1fr)"}>
+      <GridItem colSpan={1} mt="10">
+        <VStack spacing={10}>
+          <Image src={Illustration} w="400px" />
+          <Heading>Únete a una reunión</Heading>
+          <VStack>
+            <Input
+              placeholder="Código de la reunión"
+              w="200px"
+              maxLength={4}
+              bgColor="primary"
+              color="white"
+              _placeholder={{ color: "whiteAlpha.700" }}
             />
-            <PinInputField
-              borderWidth={"0 0 3px 0"}
-              borderRadius="0"
-              fontSize="xl"
-              fontWeight={"600"}
-              _focus={{ borderWidth: "0 0 3px 0" }}
-            />
-            <PinInputField
-              borderWidth={"0 0 3px 0"}
-              borderRadius="0"
-              fontSize="xl"
-              fontWeight={"600"}
-              _focus={{ borderWidth: "0 0 3px 0" }}
-            />
-            <PinInputField
-              borderWidth={"0 0 3px 0"}
-              borderRadius="0"
-              fontSize="xl"
-              fontWeight={"600"}
-              _focus={{ borderWidth: "0 0 3px 0" }}
-            />
-          </PinInput>
-        </HStack>
-        <Button onClick={() => history.push("/meet/2343")}>Entrar</Button>
-      </VStack>
+            <Button>Entrar {/*TODO: PONER ICONO */}</Button>
+          </VStack>
+        </VStack>
+      </GridItem>
 
-      <VStack
-        spacing="5"
-        textAlign={"center"}
-        w="300px"
-        h="auto"
-        borderRadius={"md"}
-        bgColor={"secondary"}
-        p="20px"
-        mt="10"
-        color="white"
-        boxShadow="rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;"
+      <GridItem
+        colSpan={1}
+        colStart={2}
+        minWidth={"400px"}
+        textAlign={"justify"}
       >
-        <Heading>Crea una nueva</Heading>
-        <Input color="white" type="text" maxLength={20} w="200px" />
-        <Button colorScheme={"purple"}>Crear</Button>
-      </VStack>
-      <Heading fontSize="3xl">
-        ESTO ES TEMPORAL PARA IR VIENDO NO ES EL DISEÑO FINAL ^^
-      </Heading>
-    </VStack>
+        <Center>
+          <VStack spacing="10" w="60%" mt="5">
+            <Heading>Crea una nueva reunión</Heading>
+            <Text fontSize={"xl"}>
+              Mucho texto muchisimo texto extremada unidad de texto
+              tremendamente largo para rellenar no te voy a engañar
+            </Text>
+            <VStack align={"left"}>
+              <Input
+                placeholder="Nombre de la reunión"
+                w="200px"
+                maxLength={4}
+                bgColor="primary"
+                color="white"
+                _placeholder={{ color: "whiteAlpha.700" }}
+              />
+              <Button>
+                Crear
+                <Icon ml="5px" />
+              </Button>
+            </VStack>
+          </VStack>
+        </Center>
+      </GridItem>
+    </Grid>
   );
 };
 
