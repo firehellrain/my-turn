d7ef11fba01effa9d3f41dd76b16d69a814208d8
firehellrain <firehellrain@gmail.com>
2022-03-31 20:41:48 +0200

    (B) Token Auth at Channels
    - Added Token Auth middleware to the websocket routing

--- myturn/myturn/asgi.py
+++ myturn/myturn/asgi.py
@@ -8,16 +8,16 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import backend.routing
+from backend.middleware.token_auth import TokenAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myturn.settings")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
+  "websocket": TokenAuthMiddleware(
         URLRouter(
             backend.routing.websocket_urlpatterns
         )
