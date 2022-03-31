7166385cb9d51a051675236cb796c56dafcb4dad
Blackwolf4183 <aperezvic21@gmai.com>
2022-03-31 09:20:24 +0200

    (F) Added redirection when accessing new meet already being moderator

--- myturn/frontend/src/Dashboard/Dashboard.jsx
+++ myturn/frontend/src/Dashboard/Dashboard.jsx
@@ -9,8 +9,9 @@ import {
   Image,
 } from "@chakra-ui/react";
 
-import React, { useState, useContext } from "react";
+import React, { useState, useContext,useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import { useMediaQuery } from "@react-hook/media-query";
 
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -27,21 +28,44 @@ const Dashboard = () => {
   const history = useHistory();
   const auth = useContext(AuthContext);
   const borderColor = useColorModeValue("black", "white");
+  const isMobile = useMediaQuery('max-width:1200px'); /* FIXME:  */
+
+  /* ON REFRESH CHECK IF USER HAS MEET */
+  const [userHasMeet, setUserHasMeet] = useState(false);
+  useEffect(() => {
+
+    console.log(isMobile);
+    let config = {
+      headers: {
+        Authorization: "Token " + auth.token,
+      },
+    };
+
+    axios
+      .get(`http://localhost:8000/backend/user_has_meet`, config)
+      .then((response) => {
+        console.log("HAS MEET: ", response.data);
+        setUserHasMeet(true);
+        setCode(response.data.meeting.meeting_id);
+      })
+      .catch((err) => {
+        console.log("has meet error: ", err);
+        setUserHasMeet(false);
+      });
+  }, [])
+  
 
   /* INPUT HANDLING FOR JOINING A MEET */
   const [code, setCode] = useState(null);
   const [loadingAccessMeet, setLoadingAccessMeet] = useState(false);
   const [errorMeetExists, setErrorMeetExists] = useState("");
+  
 
   const handleJoinMeetInput = (e) => {
     setCode(e.target.value);
   };
 
   const checkMeetExists = () => {
-
-  
-
-
     setLoadingAccessMeet(true);
 
     let config = {
@@ -50,16 +74,7 @@ const Dashboard = () => {
       },
     };
 
-
-    /* TODO: TEMPORAL */
-    /* axios.get(`http://localhost:8000/backend/user_has_meet/`, config)
-    .then(response => {
-      console.log("HAS MEET: " ,response.data);
-    })
-    .catch(err => {
-      console.log("has meet error: " , err);
-    }) */
-
+    
 
     axios
       .get(`http://localhost:8000/backend/access_meet/${code}`, config)
@@ -86,7 +101,6 @@ const Dashboard = () => {
   };
 
   const createMeet = () => {
-
     setLoadingCreateMeet(true);
     //el servidor me comprueba automaticamente si el usuario es moderador de alguna reunión
     let config = {
@@ -135,30 +149,50 @@ const Dashboard = () => {
 
         <Heading fontSize="2xl">Únete a una reunión</Heading>
         <VStack spacing="2">
-          <Input
-            placeholder="Código de la reunión"
-            fontSize="lg"
-            w="200px"
-            maxLength={4}
-            borderColor="primary"
-            borderWidth={"2px"}
-            onChange={handleJoinMeetInput}
-            isInvalid={errorMeetExists}
-          />
-
-          <MotionButton
-            w="200px"
-            bgColor={"primary"}
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.9 }}
-            transition={{ duration: 0.025 }}
-            fontSize="lg"
-            onClick={checkMeetExists}
-            isLoading={loadingAccessMeet}
-          >
-            Entrar
-            <FontAwesomeIcon style={{ marginLeft: "10px" }} icon={faUsers} />
-          </MotionButton>
+          {userHasMeet ? (
+            <MotionButton
+              w="200px"
+              bgColor={"primary"}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ duration: 0.025 }}
+              fontSize="lg"
+              onClick={checkMeetExists}
+              isLoading={loadingAccessMeet}
+            >
+              Acceder a reunión 
+              <FontAwesomeIcon style={{ marginLeft: "10px" }} icon={faUsers} />
+            </MotionButton>
+          ) : (
+            <>
+              <Input
+                placeholder="Código de la reunión"
+                fontSize="lg"
+                w="200px"
+                maxLength={4}
+                borderColor="primary"
+                borderWidth={"2px"}
+                onChange={handleJoinMeetInput}
+                isInvalid={errorMeetExists}
+              />
+              <MotionButton
+                w="200px"
+                bgColor={"primary"}
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.9 }}
+                transition={{ duration: 0.025 }}
+                fontSize="lg"
+                onClick={checkMeetExists}
+                isLoading={loadingAccessMeet}
+              >
+                Entrar
+                <FontAwesomeIcon
+                  style={{ marginLeft: "10px" }}
+                  icon={faUsers}
+                />
+              </MotionButton>{" "}
+            </>
+          )}
           {errorMeetExists && (
             <Text w="200px" textAlign={"center"} color="red.600">
               {errorMeetExists}
