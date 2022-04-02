7e786f1cfb0e9f37865a0b0001dbb518b094f6e4
Blackwolf4183 <aperezvic21@gmail.com>
2022-04-02 16:42:49 +0200

    (F) More websocket

--- myturn/frontend/src/Meet/components/MeetLayout.jsx
+++ myturn/frontend/src/Meet/components/MeetLayout.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
 
 import { Grid, GridItem } from "@chakra-ui/react";
-
-/* HOOKS */
-import { useHistory } from "react-router-dom";
 import UsersPannel from './UsersPannel';
 import Turns from './Turns';
 import UserActions from './UserActions';
+import { AuthContext } from '../../shared/context/auth-context';
+/* HOOKS */
+import { useHistory,useParams } from "react-router-dom";
+import { useEffect,useState,useContext } from 'react';
+
 
 const MeetLayout = ({meet}) => {
 
+  const auth = useContext(AuthContext);
   const history = useHistory();
+  const {mid} = useParams();
 
+  const [turns, setTurns] = useState(null);
 
   /* WEBSOCKET */
-  const ws = new WebSocket("ws://localhost:8000/ws/");
-  
+
+  const ws = new WebSocket(`ws://localhost:8000/ws/join_meet/${mid}`);
+  //al refrescar, cogemos los turnos actuales
+/* 
+  const identifierCall = {
+    data:{resquest: "get_turn_list",token_key:auth.token }
+  }
+
+
+  ws.onopen = (event) => {
+    ws.send(identifierCall);
+  }
+ */
+
 
    /* USER LEAVE */
    const handleUserLeave = () => {
