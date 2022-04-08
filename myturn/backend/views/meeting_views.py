148ebc5a51b1da3ce6dbe1e738ff115a695f0e4e
firehellrain <firehellrain@gmail.com>
2022-04-08 14:45:37 +0200

    (B) Removed unused view
    - Removed change_mod view as it was implemented on the websocket

--- myturn/backend/views/meeting_views.py
+++ myturn/backend/views/meeting_views.py
@@ -70,25 +70,3 @@ def delete_meet(request):
         meeting.delete()
         return response("Se ha cerrado correctamente la reunión", status.HTTP_200_OK)
     except: return response("El usuario no es moderador en ninguna reunión", status.HTTP_400_BAD_REQUEST)
-
-@api_view(('POST',))
-@permission_classes([IsAuthenticated])
-def change_mod(request):
-    """
-        Cambia al moderador de la reunión actual.
-        Si el usuario no es moderador de ninguna reunión,
-        devuelve un error avisando. Si el usuario para asignar
-        como moderador no existe, devuelve otro error.
-    """
-    try:
-        meeting = Meeting.objects.get(meeting_mod=request.user)
-        new_mod = User.objects.filter(username=request.data.get('new_mod'))
-        if new_mod.exists():
-            try:
-                Meeting.objects.get(meeting_mod=new_mod)
-                meeting.meeting_mod = new_mod
-                meeting.save()
-                return response("Moderador cambiado correctamente", status.HTTP_200_OK)
-            except: return response("El usuario solicitado ya es moderador en otra reunión", status.HTTP_400_BAD_REQUEST)
-        else: return response("El usuario solicitado no existe", status.HTTP_400_BAD_REQUEST)
-    except: return response("El usuario no es moderador de ninguna reunión", status.HTTP_400_BAD_REQUEST)
